cfb296f690ebaefdc67111b181f2e222f5570624
sean9061 <sean.2005sfsf@gmail.com>
2024-10-23 21:28:06 +0900

    remove commentout

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const maxSize = Math.min(80, 900 / window.innerWidth * 100); // 80vwと900pxの最小値を計算
+const maxSize = Math.min(80, 900 / window.innerWidth * 100);
 let isColonVisible = true;
 
 function updateTime() {
@@ -7,7 +7,6 @@ function updateTime() {
     const minutes = String(now.getMinutes()).padStart(2, '0');
     const seconds = String(now.getSeconds()).padStart(2, '0');
     
-    // コロンの点滅処理
     const colon = isColonVisible ? ':' : '&nbsp';
     document.getElementById('current-hours').innerHTML = hours;
     document.getElementById('current-colon').innerHTML = colon;
@@ -15,12 +14,10 @@ function updateTime() {
     
     isColonVisible = !isColonVisible;
     
-    // アスペクト比の計算
     const aspectBox = document.getElementById('aspect-box');
     const width = hours * 10;
     const height = seconds * 10;
 
-    // アスペクト比を維持しつつ、ボックスのサイズを設定
     const aspectRatio = width / height;
     if (aspectRatio > 1) {
         aspectBox.style.width = `${maxSize}vw`;
@@ -31,4 +28,4 @@ function updateTime() {
     }
 }
 setInterval(updateTime, 1000);
-updateTime(); // 初回呼び出し
+updateTime();
